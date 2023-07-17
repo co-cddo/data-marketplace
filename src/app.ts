ba0dd974ec3a24d5d245ec985739c237eded7732
duncan <duncanuk@gmail.com>
2023-07-17 09:02:15 +0100

    adds helmet security

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
+import Helmet from "helmet";
 
 const app = express();
+app.use(Helmet());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("TS auto changes through docker");
