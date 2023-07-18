22c58327669531f874d32c5a0a997c6320c5bb89
Maddie Williams <maddie.williams@tpximpact.com>
2023-07-18 15:27:10 +0100

    Nunjucks X Typescript configuration

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
 import express from "express";
+import path from "path";
+import nunjucks from "nunjucks";
 import Helmet from "helmet";
 import homeRoute from "./routes/homeRoute";
 
 export const app = express();
 app.use(Helmet());
+
+// Set static folder middleware
+app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
+// Serve the assets from the GOV.UK Frontend assets folder - recommended on the GOV UK docs
+app.use(
+  "/assets",
+  express.static(
+    path.join(__dirname, "/node_modules/govuk-frontend/govuk/assets"),
+  ),
+);
+
+// Configure Nunjucks
+nunjucks.configure(["node_modules/govuk-frontend/", "views"], {
+  autoescape: true, // prevents cross-site scripting attacks (XSS)
+  express: app,
+});
+
+// Set Nunjucks as the Express view engine
+app.set("view engine", "njk");
 
 app.use("/", homeRoute);
 
