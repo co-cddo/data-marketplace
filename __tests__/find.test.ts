db23aa5e8d2c81c4a6f39806f64ab3318584e363
duncan <duncanuk@gmail.com>
2023-08-01 20:50:50 +0100

    some renaming

--- __tests__/find.test.ts
+++ __tests__/find.test.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { fetchData } from '../src/services/findService';
-import mockData from './mock/mockData.json';
+import { fetchData } from "../src/services/findService";
+import mockData from "./mock/mockData.json";
 import { ApiResponse, Resource, DataService } from "../src/models/dataModels";
 
 function processMockData(data: ApiResponse[]): Resource[] {
@@ -21,13 +21,13 @@ function processMockData(data: ApiResponse[]): Resource[] {
 }
 
 // Mock axios get function
-jest.mock('axios');
+jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-describe('fetchData', () => {
+describe("fetchData", () => {
   beforeEach(() => {
     // Set up the axios get mock before each test
-    process.env.API_ENDPOINT = 'http://mock-test.endpoint.com/test-api';
+    process.env.API_ENDPOINT = "http://mock-test.endpoint.com/test-api";
     mockedAxios.get.mockResolvedValue({ data: mockData });
   });
 
@@ -37,29 +37,33 @@ describe('fetchData', () => {
     delete process.env.API_ENDPOINT;
   });
 
-  it('should return the correct data when no query is provided', async () => {
+  it("should return the correct data when no query is provided", async () => {
     const expectedData = processMockData(mockData);
     const result = await fetchData();
-  
+
     expect(result).toEqual(expectedData);
     expect(mockedAxios.get).toHaveBeenCalledWith(process.env.API_ENDPOINT);
   });
-  
-  it('should return filtered data when a query is provided', async () => {
-    const query = 'test';
+
+  it("should return filtered data when a query is provided", async () => {
+    const query = "test";
     const expectedData = processMockData(mockData).filter((dataService) => {
       return Object.values(dataService).some(
         (value) => value?.toString().toLowerCase().includes(query),
       );
     });
-  
+
     const result = await fetchData(query);
     expect(result).toEqual(expectedData);
     expect(mockedAxios.get).toHaveBeenCalledWith(process.env.API_ENDPOINT);
   });
 
-  it('should throw an error when the axios request fails', async () => {
-    mockedAxios.get.mockRejectedValue(new Error('An error occurred while fetching data from the API'));
-    await expect(fetchData()).rejects.toThrow('An error occurred while fetching data from the API');
+  it("should throw an error when the axios request fails", async () => {
+    mockedAxios.get.mockRejectedValue(
+      new Error("An error occurred while fetching data from the API"),
+    );
+    await expect(fetchData()).rejects.toThrow(
+      "An error occurred while fetching data from the API",
+    );
   });
 });
