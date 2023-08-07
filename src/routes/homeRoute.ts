eea98f8b3db646dba4b369cfdced26e97ecf58d9
duncan <duncanuk@gmail.com>
2023-08-07 19:06:19 +0100

    removes logs

--- src/routes/homeRoute.ts
+++ src/routes/homeRoute.ts
@@ -2,8 +2,6 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  // console.log("AM I AUTHED?", await req.isAuthenticated);
-  // console.log(req.session);
   res.render("home.njk", { route: "home" });
 });
 
