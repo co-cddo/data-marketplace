b4d08af83848f74b989899436b7b5c9179ca4ebc
Kiran Joshi <kiran@swirrl.local>
2023-08-08 20:44:15 +0100

    Adds /logout to loginRoutes
    The /logout route removes the jwtToken cookie and redirects to the index

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -3,12 +3,22 @@ const router = express.Router();
 import passport from "passport";
 
 router.get(
-  "/",
+  "/login",
   passport.authenticate("custom-sso"),
   (req: Request, res: Response, next: NextFunction) => {
     res.cookie("jwtToken", false, { httpOnly: true });
     next();
   },
 );
+
+router.get(
+  '/logout',
+  (req: Request, res: Response, next: NextFunction) => {
+    req.logout(function (err) {
+      if (err) { return next(err); }
+      res.clearCookie("jwtToken");
+      res.redirect('/');
+    });
+  });
 
 export default router;
