0ab6379785f6244599641eac7fd811c3e3f4c8d5
AShaww <71328022+AShaww@users.noreply.github.com>
2023-08-10 11:22:53 +0100

    added /Resource:ID/acquirer route

--- src/routes/shareRoutes.ts
+++ src/routes/shareRoutes.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from "express";
+import { fetchResourceById } from "../services/findService";
 const router = express.Router();
 
-router.get("/acquirer", (req: Request, res: Response) => { // initial start screen before questions
+router.get("/:resourceID/acquirer", async (req: Request, res: Response) => {
   const backLink = req.headers.referer || "/";
+  const resourceID = req.params.resourceID;
+  const resource = await fetchResourceById(resourceID);
+  console.log('Share route - Resource ID:', resourceID);
   res.render("share.njk", {
     route: req.params.page,
     heading: "Share journey",
     backLink: backLink,
+    resource: resource,
+    resourceID: resourceID,
   });
 });
 
