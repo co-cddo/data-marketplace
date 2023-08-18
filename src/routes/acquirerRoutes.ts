e42c9fd5a1491d573257dafdc9369c556d75f627
Maddie Williams <maddie.williams@tpximpact.com>
2023-08-18 11:36:09 +0100

    LegalAdvice page, linting + saved value

--- src/routes/acquirerRoutes.ts
+++ src/routes/acquirerRoutes.ts
@@ -1,29 +1,33 @@
 import express, { Request, Response } from "express";
 import { fetchResourceById } from "../services/findService";
 const router = express.Router();
-import formTemplate from "../models/shareRequestTemplate.json"
+import formTemplate from "../models/shareRequestTemplate.json";
 import { randomUUID } from "crypto";
 import { Step } from "../types/express";
 
 function parseJwt(token: string) {
-  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+  return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
 }
 
-const generateFormTemplate = (req: Request, resourceID: string, assetTitle: string) => {
+const generateFormTemplate = (
+  req: Request,
+  resourceID: string,
+  assetTitle: string,
+) => {
   const userInfo = req.user ? parseJwt(req.user.idToken) : null;
-  const username = userInfo ? userInfo.email : 'anonymous';
+  const username = userInfo ? userInfo.email : "anonymous";
   const template = JSON.parse(JSON.stringify(formTemplate));
   template.ownedBy = username;
   template.dataAsset = resourceID;
   template.requestId = randomUUID();
   template.assetTitle = assetTitle;
   return template;
-}
+};
 interface RequestBody {
   [key: string]: string | undefined;
 }
 
-const extractFormData = (stepData: Step, body: RequestBody ) => {
+const extractFormData = (stepData: Step, body: RequestBody) => {
   // Return something that will get set in the 'value' key of the form step
   // Will need to something different depending on whether the input is a radio button
   //  or text field or checkbox etc.
@@ -31,41 +35,81 @@ const extractFormData = (stepData: Step, body: RequestBody ) => {
   // All simple radio button-style forms:
   // (As long as the radio group has a name the same as the step id
 
-  const radioFields = ['data-type', 'data-access', 'legal-power'];
+  const radioFields = ["data-type", "data-access", "legal-power"];
   if (radioFields.includes(stepData.id)) {
-    return body[stepData.id]
+    return body[stepData.id];
   }
 
-  const textFields = ['data-subjects', 'data-required', 'impact', 'legal-power-textarea']; // add step names here if using textarea
+  const textFields = [
+    "data-subjects",
+    "data-required",
+    "impact",
+    "legal-power-textarea",
+  ]; // add step names here if using textarea
 
-  if (stepData.id === 'project-aims') {
+  if (stepData.id === "project-aims") {
     return {
-      aims: body['aims'] || '',
-      explanation: body['explanation'] || ''
+      aims: body["aims"] || "",
+      explanation: body["explanation"] || "",
     };
   } else {
     if (textFields.includes(stepData.id)) {
-      return body[stepData.id]
-    } 
-  }
-
-  if(stepData.id === 'benefits') {
-    return {
-      'decision-making': {explanation: body['decision-making'], checked: body['benefits']?.includes('decision-making') },
-      'service-delivery': {explanation: body['service-delivery'], checked: body['benefits']?.includes('service-delivery')},
-      'benefit-people': {explanation: body['benefit-people'], checked: body['benefits']?.includes('benefit-people')},
-      'allocate-and-evaluate-funding': {explanation: body['allocate-and-evaluate-funding'], checked: body['benefits']?.includes('allocate-and-evaluate-funding')},
-      'social-economic-trends': {explanation: body['social-economic-trends'], checked: body['benefits']?.includes('social-economic-trends')},
-      'needs-of-the-public': {explanation: body['needs-of-the-public'], checked: body['benefits']?.includes('needs-of-the-public')},
-      'statistical-information': {explanation: body['statistical-information'], checked: body['benefits']?.includes('statistical-information')},
-      'existing-research-or-statistics': {explanation: body['existing-research-or-statistics'], checked: body['benefits']?.includes('existing-research-or-statistics')},
-      'something-else': {explanation: body['something-else'], checked: body['benefits']?.includes('something-else')},
+      return body[stepData.id];
     }
   }
 
+  if (stepData.id === "benefits") {
+    return {
+      "decision-making": {
+        explanation: body["decision-making"],
+        checked: body["benefits"]?.includes("decision-making"),
+      },
+      "service-delivery": {
+        explanation: body["service-delivery"],
+        checked: body["benefits"]?.includes("service-delivery"),
+      },
+      "benefit-people": {
+        explanation: body["benefit-people"],
+        checked: body["benefits"]?.includes("benefit-people"),
+      },
+      "allocate-and-evaluate-funding": {
+        explanation: body["allocate-and-evaluate-funding"],
+        checked: body["benefits"]?.includes("allocate-and-evaluate-funding"),
+      },
+      "social-economic-trends": {
+        explanation: body["social-economic-trends"],
+        checked: body["benefits"]?.includes("social-economic-trends"),
+      },
+      "needs-of-the-public": {
+        explanation: body["needs-of-the-public"],
+        checked: body["benefits"]?.includes("needs-of-the-public"),
+      },
+      "statistical-information": {
+        explanation: body["statistical-information"],
+        checked: body["benefits"]?.includes("statistical-information"),
+      },
+      "existing-research-or-statistics": {
+        explanation: body["existing-research-or-statistics"],
+        checked: body["benefits"]?.includes("existing-research-or-statistics"),
+      },
+      "something-else": {
+        explanation: body["something-else"],
+        checked: body["benefits"]?.includes("something-else"),
+      },
+    };
+  }
+
+  if (stepData.id === "legal-power") {
+    return {
+      "legal-power-textarea": {
+        explanation: body["legal-power-textarea"],
+        checked: body["legal-power"]?.includes("legal-power-textarea"),
+      },
+    };
+  }
   // Other input types can go here
-  return
-}
+  return;
+};
 
 router.get("/:resourceID/start", async (req: Request, res: Response) => {
   const backLink = req.headers.referer || "/";
@@ -73,7 +117,7 @@ router.get("/:resourceID/start", async (req: Request, res: Response) => {
 
   try {
     const resource = await fetchResourceById(resourceID);
-   
+
     if (!resource) {
       res.status(404).send("Resource not found");
       return;
@@ -81,7 +125,9 @@ router.get("/:resourceID/start", async (req: Request, res: Response) => {
     const assetTitle = resource.title;
     // Generate a new set of form data if there wasn't one already in the session
     req.session.acquirerForms = req.session.acquirerForms || {};
-    req.session.acquirerForms[resourceID] = req.session.acquirerForms?.[resourceID] || generateFormTemplate(req, resourceID, assetTitle);
+    req.session.acquirerForms[resourceID] =
+      req.session.acquirerForms?.[resourceID] ||
+      generateFormTemplate(req, resourceID, assetTitle);
 
     res.render("../views/acquirer/start.njk", {
       route: req.params.page,
@@ -90,7 +136,7 @@ router.get("/:resourceID/start", async (req: Request, res: Response) => {
       resource: resource,
       assetTitle,
       resourceID: resourceID,
-      formdata: req.session.acquirerForms[resourceID]
+      formdata: req.session.acquirerForms[resourceID],
     });
   } catch (error) {
     console.error("An error occurred while fetching data from the API:", error);
@@ -100,15 +146,15 @@ router.get("/:resourceID/start", async (req: Request, res: Response) => {
 
 router.get("/:resourceID/:step", async (req: Request, res: Response) => {
   const resourceID = req.params.resourceID;
-  const formStep = req.params.step
+  const formStep = req.params.step;
 
   if (!req.session.acquirerForms?.[resourceID]) {
-    return res.redirect(`/share/${resourceID}/acquirer`)
+    return res.redirect(`/share/${resourceID}/acquirer`);
   }
-  
-  const formdata = req.session.acquirerForms[resourceID]
-  const stepData = formdata.steps[formStep]
-  const assetTitle = formdata.assetTitle
+
+  const formdata = req.session.acquirerForms[resourceID];
+  const stepData = formdata.steps[formStep];
+  const assetTitle = formdata.assetTitle;
 
   res.render(`../views/acquirer/${formStep}.njk`, {
     requestId: formdata.requestId,
@@ -116,20 +162,18 @@ router.get("/:resourceID/:step", async (req: Request, res: Response) => {
     assetTitle,
     stepId: formStep,
     savedValue: stepData.value,
-  })
-
+  });
 });
 
 router.post("/:resourceID/:step", async (req: Request, res: Response) => {
   if (!req.session.acquirerForms) {
     return res.status(400).send("Acquirer forms not found in session");
   }
-  
+
   const resourceID = req.params.resourceID;
   const formStep = req.params.step;
   const formdata = req.session.acquirerForms[resourceID];
   const stepData = formdata.steps[formStep];
-
 
   if (!formdata || !formdata.steps[formStep]) {
     return res.status(400).send("Form data or step not found");
@@ -149,9 +193,10 @@ router.post("/:resourceID/:step", async (req: Request, res: Response) => {
   stepData.value = extractFormData(stepData, req.body) || "";
   stepData.status = "COMPLETED";
 
-
   if (formdata.steps[formStep].nextStep) {
-    return res.redirect(`/acquirer/${resourceID}/${formdata.steps[formStep].nextStep}`);
+    return res.redirect(
+      `/acquirer/${resourceID}/${formdata.steps[formStep].nextStep}`,
+    );
   } else {
     // Handle case when nextStep is not defined
     return res.redirect(`/acquirer/${resourceID}/start`);
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -19,25 +19,25 @@ interface FormData {
 
 interface Benefits {
   explanation?: string;
-  checked?: boolean
+  checked?: boolean;
 }
 
 type BenefitsStep = {
-  'decision-making'?: Benefits;
-  'service-delivery'?: Benefits;
-  'benefit-people'?: Benefits;
-  'allocate-and-evaluate-funding'?: Benefits;
-  'social-economic-trends'?: Benefits;
-  'needs-of-the-public'?: Benefits;
-  'statistical-information'?: Benefits;
-  'existing-research-or-statistics'?: Benefits;
-  'something-else'?: Benefits;
-}
+  "decision-making"?: Benefits;
+  "service-delivery"?: Benefits;
+  "benefit-people"?: Benefits;
+  "allocate-and-evaluate-funding"?: Benefits;
+  "social-economic-trends"?: Benefits;
+  "needs-of-the-public"?: Benefits;
+  "statistical-information"?: Benefits;
+  "existing-research-or-statistics"?: Benefits;
+  "something-else"?: Benefits;
+};
 
-type ProjectAimStep = { 
-  aims: string; 
+type ProjectAimStep = {
+  aims: string;
   explanation: string;
-}
+};
 
 type StepValue = string | ProjectAimStep | BenefitsStep;
 
