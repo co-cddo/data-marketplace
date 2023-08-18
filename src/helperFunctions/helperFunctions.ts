4ffedde1f6d417382c0e0977dafd997ba1e63985
AShaww <71328022+AShaww@users.noreply.github.com>
2023-08-18 14:41:22 +0100

    Implemented legal-review page

--- src/helperFunctions/helperFunctions.ts
+++ src/helperFunctions/helperFunctions.ts
@@ -114,7 +114,7 @@ const extractFormData = (stepData: Step, body: RequestBody) => {
   // All simple radio button-style forms:
   // (As long as the radio group has a name the same as the step id
 
-  const radioFields = ["data-type", "data-access"];
+  const radioFields = ["data-type", "data-access", "legal-review"];
   if (radioFields.includes(stepData.id)) {
     return body[stepData.id];
   }
