547673d69e835c1bf5c6d378bf9626bfda660952
AShaww <71328022+AShaww@users.noreply.github.com>
2023-08-29 22:39:00 +0100

    display colours of tags based on status value

--- src/routes/supplierRoutes.ts
+++ src/routes/supplierRoutes.ts
@@ -11,11 +11,25 @@ router.get("/", async (req: Request, res: Response) => {
 router.get("/created-requests", async (req: Request, res: Response) => {
   const acquirerForms = req.session.acquirerForms || {};
   const backLink = req.headers.referer || "/";
-  
   res.render("../views/supplier/created-requests.njk", {
     backLink,
-    acquirerForms
+    acquirerForms,
+    getStatusClass
   });
 });
+
+// Function to get the tag class based on the status value
+function getStatusClass(status: string): string {
+  switch (status) {
+    case "NOT STARTED":
+      return "govuk-tag--grey";
+    case "IN PROGRESS":
+      return "govuk-tag--blue";
+    case "RETURNED":
+      return "govuk-tag--red";
+    default:
+      return "govuk-tag--grey";
+  }
+}
 
 export default router;
