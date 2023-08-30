6f21909ba977674cef9f61f36c354b413d81496e
AShaww <71328022+AShaww@users.noreply.github.com>
2023-08-30 09:16:11 +0100

    abstracted date rendering to string in template

--- src/routes/supplierRoutes.ts
+++ src/routes/supplierRoutes.ts
@@ -1,22 +1,7 @@
 import express, { Request, Response } from "express";
+import { DateStep } from "../types/express";
 const router = express.Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  const backLink = req.headers.referer || "/";
-  res.render("../views/supplier/manage-shares.njk", {
-    backLink,
-  });
-});
-
-router.get("/created-requests", async (req: Request, res: Response) => {
-  const acquirerForms = req.session.acquirerForms || {};
-  const backLink = req.headers.referer || "/";
-  res.render("../views/supplier/created-requests.njk", {
-    backLink,
-    acquirerForms,
-    getStatusClass
-  });
-});
 
 // Function to get the tag class based on the status value
 function getStatusClass(status: string): string {
@@ -31,5 +16,49 @@ function getStatusClass(status: string): string {
       return "govuk-tag--grey";
   }
 }
+
+router.get("/", async (req: Request, res: Response) => {
+  const backLink = req.headers.referer || "/";
+  res.render("../views/supplier/manage-shares.njk", {
+    backLink,
+  });
+});
+
+router.get("/created-requests", async (req: Request, res: Response) => {
+  const acquirerForms = req.session.acquirerForms || {};
+  const backLink = req.headers.referer || "/";
+
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const tableRows = [];
+
+  for (const [key, formData] of Object.entries(acquirerForms)) {
+    let formattedDate = "Unrequested";
+    const dateValue = formData.steps.date.value as DateStep;
+    key === formData.requestId;
+
+    if (dateValue.day && dateValue.month && dateValue.year) {
+      const monthIndex = dateValue.month - 1;
+      const monthName = monthNames[monthIndex];
+      formattedDate = `${dateValue.day} ${monthName} ${dateValue.year}`;
+    }
+    const row = [
+      { html: `<a href="/acquirer/${formData.requestId}/summary">${formData.requestId}</a>` },
+      { text: formData.assetTitle },
+      { text: formData.ownedBy },
+      { text: formattedDate },
+      { html: `<span class="govuk-tag ${getStatusClass(formData.status)}">${formData.status}</span>` }
+    ];
+
+    tableRows.push(row);
+  }
+
+  res.render("../views/supplier/created-requests.njk", {
+    backLink,
+    acquirerForms,
+    getStatusClass,
+    tableRows
+  });
+});
+
 
 export default router;
