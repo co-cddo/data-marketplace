229d720798b8ca93f10e42f59d64a5d0dcc99c15
Maddie Williams <maddie.williams@tpximpact.com>
2023-09-04 09:42:52 +0100

    Additional temporary routing

--- src/routes/supplierRoutes.ts
+++ src/routes/supplierRoutes.ts
@@ -125,22 +125,25 @@ router.get("/decision", async (req: Request, res: Response) => {
   });
 });
 
-router.post('/decision', async (req: Request, res: Response) => {
+router.post("/decision", async (req: Request, res: Response) => {
   const decision = req.body.decision;
 
-  if (decision === 'approve') {
-    return res.redirect('/manage-shares/declaration');
+  if (decision === "approve") {
+    return res.redirect("/manage-shares/declaration");
   }
 
-  return res.redirect('/manage-shares/received-requests');
+  return res.redirect("/manage-shares/received-requests");
 });
-
 
 router.get("/declaration", async (req: Request, res: Response) => {
   const backLink = req.headers.referer || "/";
   res.render("../views/supplier/declaration.njk", {
     backLink,
   });
+});
+
+router.post("/declaration", async (req: Request, res: Response) => {
+  return res.redirect("/manage-shares/received-requests");
 });
 
 export default router;
