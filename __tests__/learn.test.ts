3b572e626a2e37f7d06cb30f0e538b8c0035deb9
gabimartini <gabimartinii@hotmail.com>
2023-09-05 15:48:41 +0100

    add success test

--- __tests__/learn.test.ts
+++ __tests__/learn.test.ts
@@ -5,5 +5,6 @@ describe("GET /learn", () => {
   it('should respond as expected"', async () => {
     const response = await request(app).get("/learn");
     expect(response.status).toBe(200);
+    expect(response.text).toContain("Data Marketplace");
   });
 });
