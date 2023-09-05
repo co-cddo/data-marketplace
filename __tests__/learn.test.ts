2e01cf97cc7a57881156d0e4569e759373a4e21f
gabimartini <gabimartinii@hotmail.com>
2023-09-05 15:47:12 +0100

    add success test

---
+++ __tests__/learn.test.ts
@@ -0,0 +1,9 @@
+import request from "supertest";
+import app from "../src/app";
+
+describe("GET /learn", () => {
+  it('should respond as expected"', async () => {
+    const response = await request(app).get("/learn");
+    expect(response.status).toBe(200);
+  });
+});
