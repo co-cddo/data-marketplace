9c75a6bdd5ca18d0cae97805d65da71959e2c7f0
Maddie Williams <maddie.williams@tpximpact.com>
2023-09-08 09:38:59 +0100

    fix merge conflicts

--- src/routes/learnRoutes.ts
+++ src/routes/learnRoutes.ts
@@ -18,6 +18,7 @@ router.get("/data-sharing-questions", async (req: Request, res: Response) => {
 
 router.get("/adding-a-single-data-asset", async (req: Request, res: Response) => {
   res.render("../views/learn/adding-a-single-data-asset.njk");
+});
 
 router.get("/guidance-on-publish", async (req: Request, res: Response) => {
   res.render("../views/learn/guidance-on-publish.njk");
