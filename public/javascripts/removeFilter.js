583e5b480b9b1a9e4d2898f74fd089f81501e701
duncan <duncanuk@gmail.com>
2023-09-23 07:54:17 +0100

    adds js implementation of back

--- public/javascripts/removeFilter.js
+++ public/javascripts/removeFilter.js
@@ -1,25 +1,36 @@
 function removeFilter() {
-  document.addEventListener('DOMContentLoaded', () => {
-    const filterButtons = document.querySelectorAll('.moj-filter__tag');
+  document.addEventListener("DOMContentLoaded", () => {
+    const backButton = document.getElementById("history-back-link");
+    if (backButton) {
+      backButton.addEventListener("click", (event) => {
+        console.log("back clicked");
+        event.preventDefault();
+        window.history.back();
+      });
+    }
 
-    filterButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            const target = event.target;
-            const filterValue = target.getAttribute('data-filter-value');
-            const filterCategory = target.getAttribute('data-filter-category');
+    const filterButtons = document.querySelectorAll(".moj-filter__tag");
 
-            // Find the corresponding checkbox and uncheck it
-            const checkbox = document.querySelector(`input[name="${filterCategory}"][value="${filterValue}"]`);
-            if (checkbox) {
-                checkbox.checked = false;
-            }
+    filterButtons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        const target = event.target;
+        const filterValue = target.getAttribute("data-filter-value");
+        const filterCategory = target.getAttribute("data-filter-category");
 
-            // Re-submit the form
-            const form = document.getElementById('searchForm');
-            if (form) {
-                form.submit();
-            }
-        });
+        // Find the corresponding checkbox and uncheck it
+        const checkbox = document.querySelector(
+          `input[name="${filterCategory}"][value="${filterValue}"]`,
+        );
+        if (checkbox) {
+          checkbox.checked = false;
+        }
+
+        // Re-submit the form
+        const form = document.getElementById("searchForm");
+        if (form) {
+          form.submit();
+        }
+      });
     });
   });
 }
