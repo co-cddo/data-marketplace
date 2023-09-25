bc1f6fee027054a7d71243c642758ef1c9588974
Maddie Williams <maddie.williams@tpximpact.com>
2023-09-25 11:45:37 +0100

    Remove comment

--- public/javascripts/backLink.js
+++ public/javascripts/backLink.js
@@ -1,6 +1,5 @@
 function backLink() {
   document.addEventListener("DOMContentLoaded", () => {
-  // leaving this commented in favour of backend solution
   const backButton = document.getElementById("history-back-link");
   if (backButton) {
     backButton.addEventListener("click", (event) => {
